1bacca0c2bcf13458cd9d8f8fe0366f38bfe0bf7
yparmar2080 <yuvrajparmar2080@gmail.com>
2020-09-28 15:20:43 -0400

    github conversion

---
+++ src/script.js
@@ -0,0 +1,196 @@
+/** 
+ *  
+  Name: Yuvraj Parmar
+  ID: 121391171
+  Class: Web222
+  Reason: Final project/assessment
+  Date: April 16, 2020
+  Professor: David Humphrey
+ * Add any JavaScript you need to this file. */
+window.itemsData = [
+    {
+        itemName: 'Gigabye Geforce GTX 1080',
+        category: 'Component',
+        price: '$599.99',
+        description: "This a Gigabyte Geforce GTX 1080 OC Windforce edition graphics card. It packs 8GB of GDDR5x vram, connects via PCI Express 3.0, supports 3 Display ports, 1 HDMI port and 1 DVI-D port. Has 2560 CUDA Cores and a baseclock of 1657 MHz. This graphics card packs a strong punch in gaming, optimal for high refresh rate and high resolution gaming and high settings. Don't spare a dime when it comes to performance this great.",
+        image: 'images/GTX1080.jpg',
+        rotate: "0"
+    },
+    {
+        itemName: 'Zotac AMP! Geforce GTX 1060',
+        category: 'Component',
+        price: '$299.99',
+        description: "This is the Zotac Geforce 6GB GTX 1060 AMP! edition graphics card. The GTX 1060 is known as the 1080p god as it can almost certainly push 60 frames per second on almost every triple A game out to date. With a base clock of 1556MHz and a boost clock of 1797MHz, blow away the competition. It's dual fan design allows for optimal cooling even under the toughest of loads.",
+        image: 'images/GTX1060.jpg',
+        rotate: "0"
+    },{
+        itemName: 'Intel i5-4430',
+        category: 'Component',
+        price: '$149.99',
+        description: "Intel's Haswell i5-4430 processor is a 4 core 4 threads processor. It fits in LGA1150 sockets and comes with a stock cooler. Can go up to 3.20 GHz with a TDP of 84 watts. This processor supports DDR3 ram up to an amount of 32GB. This product ships with Intel HD Graphics 4600 on board.",
+        image: 'images/i5-4430.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'Intel i5-6600k',
+        category: 'Component',
+        price: '$249.99',
+        description: "Intel's SkyLake i5-6600k processor is a 4 core 4 threads processor. It's base clock is 3.50 GHz, with a turbo boost of 3.90 GHz and fits into LGA1151 sockets. Paired with a Z170 chipset motherboard, this processor can be overlocked reaching up to speeds of 4.50 GHz and higher. This processor supports DDR4 ram up to 64GB.",
+        image: 'images/6600k.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'Intel Pentium G3420',
+        category: 'Component',
+        price: '$69.99',
+        description: "This is Intel's Haswell Pentium G3420 dual core processor. Going up to speeds of 3.2GHz, it features Intel HD graphics and supports up to 32 GB of DDR3 ram. It has a TDP of 91 watts.",
+        image: 'images/G3420.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'Intel Pentium G3258',
+        category: 'Component',
+        price: '$99.99',
+        description: "This is Intel's Haswell Pentium G3258 dual core processor. With a base speed of 3.20 GHz, it can support up to 32 GB of DDR3 ram with a TDP of 53 watts. It ships with integrated Intel HD Graphics.",
+        image: 'images/G3258.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'Samsung 860 QVO 2TB SSD',
+        category: 'Component',
+        price: '$199.99',
+        description: "This is Samsung's 860 QVO 2TB solid state drive. Delivers fast read/write speeds of 550/520 MB/s.There is enough storage for all your files with high speed transfer rates.",
+        image: 'images/860QVOSSD.jpg',
+        rotate: "0"
+    },{
+        itemName: 'GamerStorm Captain 120EX AIO Water Cooler',
+        category: 'Component',
+        price: '$79.99',
+        description: "This is GamerStorm's Captain 120EX All in One water cooler. It is compatible with most consumer grade cpu sockets. It comes with a fan and has a red light on the cpu plate tube. It supports a single 120mm system fan on it's radiator.",
+        image: 'images/Captain120EX.jpg',
+        rotate: "0"
+    },{
+        itemName: 'EVGA 600B 80Bronze Power Supply',
+        category: 'Component',
+        price: '$69.99',
+        description: "EVGA 600B is a non modular 600 watt power supply for computers. It has a fan at the top of the powersupply. This high quality power supply has all the cables included for the majority of consumer grade PCs",
+        image: 'images/EVGA600B.jpg',
+        rotate: "0"
+    },{
+        itemName: 'Logitech G502 Proteus Core',
+        category: 'Accessory',
+        price: '$49.99',
+        description: "Logitech's G502 Proteus core is gaming mouse with a sleek and stylish design. The logo on the mouse can be controlled to display different brightness' through the logitech software.",
+        image: 'images/G502.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'Logitech G910 Orion Spark Keyboard',
+        category: 'Accessory',
+        price: '$149.99',
+        description: "Logitech's 910 Orion Spark keyboard is a usb mechanical keyboard containing Romer G switchs and rgb per key lighting. The romer G switchs were made with gaming in mind as they took the good aspects the popular cherry mx switchs and added their own kick to it. The per key lighting can be customized in logitech's G Hub software.",
+        image: 'images/G910.jpg',
+        rotate: "0"
+    },{
+        itemName: 'Logitech G933 Artemis Spectrum Wireless Gaming Headset',
+        category: 'Accessory',
+        price: '$199.99',
+        description: "Logitech's G933 Artemis Spectrum is a wireless head set that connects via a wireless usb adapter. The G933 has rgb lighting with can be customized in logitech's G Hub software. This headset can be connected with a headphone jack as well allowed for extra versatility.",
+        image: 'images/G933.jpg',
+        rotate: "0"
+    },{
+        itemName: 'Nvidia Shield Controller',
+        category: 'Accessory',
+        price: '$79.99',
+        description: "Nvida Shield Controller is a bluetooth controller built for Nvidia shield devices. Though it can be used on android and windows devices as well via bluetooth or usb. This is a high quality built controller, which was built to last a long time.",
+        image: 'images/NvidiaShieldController.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'Asus USB-AC56 Wireless Adapter',
+        category: 'Accessory',
+        price: '$79.99',
+        description: "Asus's USB-AC56 Wireless Adapater is a usb adapter which allows for AC wifi connectivity. It comes with an antenna to allow for greater reach of signal. This device supports USB 3.0 which allows for faster connectivity. It is a dual band adapater supporting both 2.4ghz and 5ghz channels.",
+        image: 'images/USBAC56.jpg',
+        rotate: "rotate90"
+    },{
+        itemName: 'MSI Optix G24C LED Gaming Monitor',
+        category: 'Accessory',
+        price: '$279.99',
+        description: "MSI's Optix G24C LED gaming monitor is a full HD 1080p monitor. It has a 144hz refreshrate and supports AMD freesync. It work's well with Nvidia's G-Sync as well. It is a curved 24 inch monitor built with a VA panel.",
+        image: 'images/OptixG24C.jpg',
+        rotate: "0"
+    }
+];
+
+let items = window.itemsData;
+
+let buildItemList = function(items, category) {
+    let list = document.querySelector('#items');
+    let title = document.createElement('h2');
+    title.innerHTML = `Displaying: ${category}`;
+    list.appendChild(title);
+    items.forEach(element => {
+        let nl = document.createElement('br');
+        let item = document.createElement('div');
+        item.className = 'item';
+        let img = document.createElement('img');
+        img.src = element.image;
+        img.className = element.rotate;
+        img.alt = '';
+        img.width = '175';
+        let name = document.createElement('h2');
+        name.innerHTML = element.itemName;
+        let desc = document.createElement('span');
+        desc.class = 'description';
+        desc.innerHTML = `${element.description}<br>`;
+        desc.appendChild(nl);
+        let category = document.createElement('span');
+        category.class = 'category';
+        category.innerHTML = `Category: ${element.category}<br><br>`;
+        let price = document.createElement('span');
+        price.class = 'price';
+        price.innerHTML = element.price;
+        item.appendChild(img);
+        item.appendChild(name);
+        item.appendChild(desc);
+        item.appendChild(category);
+        item.appendChild(price);
+        list.appendChild(item);
+    });
+}; 
+
+let clearItems = function() {
+    let item = document.querySelector('#items');
+    item.innerHTML = ' ';
+};
+
+let setupMenuHandlers = function() {
+    let all = document.querySelector('#all');
+    let comp = document.querySelector('#components');
+    let acc = document.querySelector('#Accessories');
+    let allClick = function() {
+        clearItems();
+        buildItemList(items, "All Items");
+    };
+    let compClick = function() {
+        let arr = getByCategory('Component');
+        clearItems();
+        buildItemList(arr, "Components");
+    };
+    let accClick = function() {
+        let arr = getByCategory('Accessory');
+        clearItems();
+        buildItemList(arr, "Accessories");
+    };
+
+    all.addEventListener('click', allClick);
+    comp.addEventListener('click', compClick);
+    acc.addEventListener('click', accClick);
+};
+
+let getByCategory = function(category){
+    let arr = [];
+    arr = items.filter(element => element.category === category);
+    return arr;
+};
+
+window.onload = function(){  
+    buildItemList(items, "All Items");
+    setupMenuHandlers(); 
+};
+
